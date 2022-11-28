55664df0ec285b3737edfd84838f7307cd01a355
jbrackin1 <92929373+jbrackin1@users.noreply.github.com>
2022-11-27 23:26:30 -0600

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,5 @@
 const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL || "https://welovemoviesfront.onrender.com/theaters";
+  process.env.REACT_APP_API_BASE_URL || "https://welovemoviesfront.onrender.com";
 
 /**
  * Defines the default headers for these functions to work with `json-server`
